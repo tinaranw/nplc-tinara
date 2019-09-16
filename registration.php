eab507473a9378b665a92912e06e50abc21924e9
tinaranw <50069062+tinaranw@users.noreply.github.com>
2019-09-17 06:11:19 +0700

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="assets/layouts/tin-flex.css">
+	<link rel="stylesheet" href="index.css">
+
+	<link href="https://unpkg.com/ionicons@4.5.10-0/dist/css/ionicons.min.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Oswald:200,300,400,500,600,700&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+	<link rel="stylesheet" type="text/css" href="assets/scripts/slick/slick.css"/>
+	<link rel="stylesheet" type="text/css" href="assets/scripts/slick/slick-theme.css"/>
+	<style>
+	</style>
+</head>
+<body>
+<div class="wrapper">
+
+    <div class="section header">
+
+        <div class="box-4 v-top h-center" style="background-color: #eac7a9;text-align: center;">
+
+            <div style="max-width:500px;" class="container-fluid regis-space">
+                
+                <div class="row">
+                    <div class="col-12">
+                        <div class="container" style="width:90%">
+                            <img src="uploads/logoedited.png">
+                        </div>
+                    </div>
+                </div>
+              
+            </div>
+        </div>
+
+        <div class="box-8 v-mid h-right" style="background-image:url(uploads/background.png);background-position: 50% 80%;">
+
+            <div style="max-width:400px;" class="container-fluid space-1">
+                
+                <div class="row">
+                    <div class="col-12">
+                    	<h1>Info Pendaftaran</h1>
+                    	<div class="card">
+                    		
+	                        <p>1. Active SMA, SMK, MA students.</p>
+							<p>2. A team consist of 1 to 3 active students from the same school (Grade X-XII).</p>
+							<p>3. Required logic skill.</p>
+							<p>4. Each school may send more than 1 team.</p>
+							<p>5. Coach is optional. A coach could conduct more than one team.</p>
+                    	</div>
+                        
+                    </div>
+                </div>
+              
+            </div>
+        </div>
+        
+    </div>
+
+</div>
+	
+
+<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+<script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<script type="text/javascript" src="assets/scripts/slick/slick.min.js"></script>
+<script type="text/javascript" src="assets/scripts/skrollr-master/dist/skrollr.min.js"></script>
+	
+<script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+<script type="text/javascript">
+
+
+$(document).ready(function(){
+
+	/*$("#menuButton").click(function(){ 
+			$("#menuDisplay").css('display','block');
+		});*/
+
+	/* AOS */
+  
+  	AOS.init({
+    	duration: 1200,
+	});
+
+});
+
+</script>
+
+</body>
+</html>
+
+
+
